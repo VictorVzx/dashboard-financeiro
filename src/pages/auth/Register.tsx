ec6d43f23695308c802f37ca352f0dd69cf10a41
Victor <gifpa45we@gmail.com>
2026-02-01 01:05:52 -0300

    feat: create register page

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,8 +1,58 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Eye, EyeOff } from "lucide-react"
+
 function Register() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     return(
-        <div>
-            <h2>AAAAAAAAAA</h2>
+        <div className="p-7 flex flex-col justify-center items-center w-full min-h-screen border-r-2 bg-gray-100">
+        
+        <div className="w-100 h-140 flex flex-col p-10 rounded-2xl shadow-2xl bg-white">
+          <h2 className="font-bold text-4xl text-center">Registrar-se</h2>
+          <p className=" mb-4 text-center text-xl">Crie a sua conta para entrar!</p>
+          <div>
+            <label htmlFor="fieldgroup-name">Nome</label>
+            <Input id="fieldgroup-name" placeholder="João Silva" />
+          </div>
+          <div>
+            <label htmlFor="fieldgroup-email">Email</label>
+            <Input
+              id="fieldgroup-email"
+              type="email"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="fieldgroup-email">Senha</label>
+            <div className="relative">
+              <Input
+                id="fieldgroup-password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Your strong password"
+                className="pr-10"
+              />
+              <Button type="button" onClick={() => setShowPassword(!showPassword)} variant="ghost" size="sm" className="absolute right-1 top-1/2 transform -translate-y-1/2 p-1"> {showPassword ? <EyeOff size={16} /> : <Eye size={16} />} </Button>
+            </div>
+            <label htmlFor="fieldgroup-confirm">Confirmar senha</label>
+            <div className="relative">
+              <Input
+                id="fieldgroup-confirm"
+                type={showConfirmPassword ? "text" : "password"}
+                placeholder="Confirm your password"
+                className="pr-10"
+              />
+              <Button type="button" onClick={() => setShowConfirmPassword(!showConfirmPassword)} variant="ghost" size="sm" className="absolute right-1 top-1/2 transform -translate-y-1/2 p-1"> {showConfirmPassword ? <EyeOff size={16} /> : <Eye size={16} />} </Button>
+            </div>
+            <div className="flex flex-col gap-2 mt-2 md:flex-row md:justify-between">
+              <p><a className="hover:underline ml-70" href="./Login">Login</a></p>
+            </div>
+          </div>
+          <div className="mt-4">
+            <Button type="submit" className="w-full cursor-pointer">Submit</Button>
+          </div>
+
         </div>
+    </div>
     )
 }
 
